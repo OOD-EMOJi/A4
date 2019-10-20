78fa196338c7978b958011dc5087b447f17c0dff
Ellie <43616100+elllie@users.noreply.github.com>
2019-10-20 10:30:33 -0500

    add location to Tile

--- Tile.java
+++ Tile.java
@@ -2,10 +2,13 @@ public class Tile {
 	
 	private Thing contents;
     private boolean isWall;
+    private int x, y;
 
-    public Tile(boolean isWall) {
+    public Tile(boolean isWall, int x, int y) {
         this.isWall = isWall;
 		this.contents = null;
+        this.x = x;
+        this.y = y;
     }
 
     public boolean isWall() {
@@ -27,6 +30,14 @@ public class Tile {
 	public void setContents(Thing contents) {
 		this.contents = contents;
 	}
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
 
 	@Override
 	public String toString() {
