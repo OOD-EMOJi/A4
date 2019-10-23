fb94307e24faa30820ad5a63171521148326dc16
Maria shehata <maria.sheahata17@ncf.edu>
2019-10-23 12:15:12 -0400

    first draft

--- DepthFirstSearchPathFinder.java
+++ DepthFirstSearchPathFinder.java
@@ -1,6 +1,6 @@
-
 import java.util.Stack;
 import java.util.ArrayList;
+import java.util.*;
 /*
           Algorithm
 
@@ -11,8 +11,8 @@ import java.util.ArrayList;
 5] 		while the stack is not empty:
 6] pop the first tile in the stack
 7] if the tile is not visited and its not a wall:
-8]		then visit the tile
-9]		for each unvisited neighbor tile  of the current tile :
+8]		then visit the 
+9]		for each unvisited neighbor of the current tile :
 			if the unvisited neighbor  is not a wall:
 10]			push the unvisited neighbor into the stack
 11]End the process when we find the "Ending Tile"
@@ -38,8 +38,8 @@ public class DepthFirstSearchPathFinder implements PathFinder{
 	public List<Tile> findPath(){
 		List<Tile> path = new ArrayList<Tile>();
 		boolean[][] visited = new boolean[width][height];
-		Stack<int[]> stack = new Stack<int[]>();
-		Tile currentTile = new Tile;
+		Stack<Tile> stack = new Stack<Tile>();
+		Tile currentTile ;
 
 		// step 3]  add the "Beginign Tile"
 		path.add(start);
@@ -52,13 +52,19 @@ public class DepthFirstSearchPathFinder implements PathFinder{
 		  };
 
 		//step 4] & 5]
+		currentTile = start;
 		while(currentTile != end){
 			while(!stack.isEmpty()){
 				currentTile = stack.pop();
 				if(!visited[currentTile.x][currentTile.y] && !currentTile.isWall()){
-					visited[currentTile.x][currentTile.y] = true
+					visited[currentTile.x][currentTile.y] = true;
 					//step 9 
 					for (int[] shift : SHIFTS) {
+						if (canTraverse(currentTile, currentTile.x +shift[0],currentTile.x +shift[1])){
+							Tile neighbor = maze.tiles[currentTile.x +shift[0]][currentTile.x + shift[1]];
+							stack.push(neighbor);
+							path.add(neighbor);
+						}
 
 
 					}
@@ -68,13 +74,13 @@ public class DepthFirstSearchPathFinder implements PathFinder{
   }
 
   /*
-  //////////// need to modify this ///////////////////
-  Validates a given tile, it ensures it is in the maze and is white (can
-  be "walked on")
+  Helper function
+  Validates a given tile, it ensures it is in the maze and ican
+  be "walked on"
 */
-	private static boolean canTraverse(Coordinate node, List<List<Color>> maze) {
-	  return node.row >= 0 && node.row < maze.size() &&
-	         node.col >= 0 && node.col < maze.get(node.row).size() &&
-	         maze.get(node.row).get(node.col) == Color.WHITE;
+	private boolean canTraverse(Tile tile, int x, int y) {
+	    return x >= 0 && x < width &&
+	  		   y >= 0 && y < height &&
+			   !tile.isWall();	
 	}
 }
