cb72961d6e8ce96e141aef3293a4e532923fa7b6
ofulcher16 <olympia.fulcher16@ncf.edu>
2019-10-23 17:00:24 -0400

    Updated Mouse.java
    Added all the methods except move()

--- Mouse.java
+++ Mouse.java
@@ -1,5 +1,31 @@
 public class Mouse implements Thing {
 	private int x;
 	private int y;
-	PathFinder pathFinder;
+	Pathfinder pathFinder;
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int newX) {
+		x = newX;
+	}
+	
+	public void setY(int newY) {
+		y = newY;
+	}
+	
+	public char draw() {
+		return 'o';
+	}
+
+	
+	public void move() {
+		//stub, will update soon
+		
+	}
 }
